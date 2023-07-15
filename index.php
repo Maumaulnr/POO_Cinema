ae6d76fd588f68672b9a7c48cfd3acc0e0651c0d
Maurane Lenoir <maurane1831@gmail.com>
2023-07-15 22:08:52 +0200

    15/07/2023

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require "Film.php";
+require "Genre.php";
+require "Acteur.php";
+require "Personne.php";
+require "Role.php";
+require "Realisateur.php";
+require "Casting.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
